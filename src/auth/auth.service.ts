7b26e4162de0b15674dcd55365c65a6b649d1341
Parsyaaa14 <muhammadparsya1@gmail.com>
2024-10-10 12:04:21 +0700

    manambahkan session storage

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -77,67 +77,6 @@ export class AuthService {
     };
   }
 
-  // async loginForKasir(
-  //   email: string,
-  //   password: string,
-  // ): Promise<{ access_token?: string; redirectUrl?: string; alert?: string }> {
-  //   const user = await this.usersRepository.findOne({
-  //     where: { email },
-  //     relations: ['role'], // Ensure role is loaded
-  //   });
-
-  //   if (!user) {
-  //     throw new UnauthorizedException('Email tidak ditemukan');
-  //   }
-
-  //   // Cek apakah role adalah 'kasir'
-  //   if (user.role.nama !== 'Kasir') {
-  //     throw new UnauthorizedException('Akses ditolak: Anda bukan kasir');
-  //   }
-
-  //   // Cek apakah status kasir adalah INACTIVE
-  //   if (user.status === 'tidak aktif') {
-  //     throw new UnauthorizedException(
-  //       'Akses ditolak: Akun Anda sedang tidak aktif',
-  //     );
-  //   }
-
-  //   // Jika password yang diinput adalah default '123456', arahkan user ke endpoint edit-password
-  //   if (password === '123456' && !user.password.includes('$2b$')) {
-  //     return {
-  //       alert: 'Anda perlu mengubah password Anda',
-  //       redirectUrl: `/edit_password_kasir?id=${user.id_user}`, // Menggunakan path yang benar
-  //     };
-  //   }
-
-  //   // Jika password sudah diubah, hash password-nya
-  //   const isMatch = await bcrypt.compare(password, user.password);
-
-  //   if (!isMatch) {
-  //     throw new UnauthorizedException('Password salah');
-  //   }
-
-  //   user.lastLogin = new Date();
-  //   await this.usersRepository.save(user);
-
-  //   // Buat payload JWT
-  //   const payload = {
-  //     email: user.email,
-  //     sub: user.id_user,
-  //     iat: Math.floor(Date.now() / 1000), // Add issued at (current time in seconds)
-  //   };
-
-  //   const access_token = this.jwtService.sign(payload);
-  //   // Kembalikan token dan redirectUrl jika password diubah
-  //   return {
-  //     access_token,
-  //     redirectUrl:
-  //       user.password === '123456'
-  //         ? `/edit_password_kasir?id=${user.id_user}`
-  //         : undefined,
-  //   };
-  // }
-
   async validateUser(token: string): Promise<User> {
     try {
       const payload = this.jwtService.verify(token);
@@ -155,50 +94,54 @@ export class AuthService {
     const user = await this.usersRepository.findOne({ where: { email } });
 
     if (!user) {
-      throw new UnauthorizedException('Akun tidak ditemukan');
+        throw new UnauthorizedException('Akun tidak ditemukan');
     }
 
     // Validasi password user
     const passwordIsValid = await bcrypt.compare(password, user.password);
     if (!passwordIsValid) {
-      throw new UnauthorizedException('Password salah');
+        throw new UnauthorizedException('Password salah');
     }
 
     // Cari toko yang terkait dengan user tersebut, gunakan user.id_user
     const toko = await this.tokoRepository.findOne({
-      where: { user: { id_user: user.id_user } },
+        where: { user: { id_user: user.id_user } },
     });
 
     if (!toko) {
-      throw new UnauthorizedException('Toko tidak ditemukan untuk user ini');
+        throw new UnauthorizedException('Toko tidak ditemukan untuk user ini');
     }
 
     // Cek status toko
     if (toko.status === StatusToko.PENDING) {
-      throw new ForbiddenException(
-        'Akun Anda masih dalam proses persetujuan. Harap tunggu.',
-      );
+        throw new ForbiddenException(
+            'Akun Anda masih dalam proses persetujuan. Harap tunggu.',
+        );
     }
 
     if (toko.status === StatusToko.REJECTED) {
-      return { redirect: '/toko-rejected-page' }; // Ganti '/toko-rejected-page' dengan path yang sesuai
+        return { redirect: '/toko-rejected-page' }; // Ganti '/toko-rejected-page' dengan path yang sesuai
     }
 
     // Generate JWT token
     const payload = {
-      email: user.email,
-      sub: user.id_user,
-      iat: Math.floor(Date.now() / 1000),
-    }; // Add issued at (current time in seconds) };
+        email: user.email,
+        sub: user.id_user,
+        iat: Math.floor(Date.now() / 1000), // Add issued at (current time in seconds)
+    };
     const access_token = this.jwtService.sign(payload);
 
+    // Store current user and current store in sessionStorage
+    sessionStorage.setItem('currentUser', JSON.stringify(user));
+    sessionStorage.setItem('currentStore', JSON.stringify(toko));
+
     if (access_token) {
-      console.log(
-        `Toko ${toko.nama_toko} berhasil login dengan status ${toko.status}`,
-      );
+        console.log(
+            `Toko ${toko.nama_toko} berhasil login dengan status ${toko.status}`,
+        );
     }
     return { message: 'Login berhasil', access_token };
-  }
+}
 
   async loginForSuperadmin(
     email: string,
